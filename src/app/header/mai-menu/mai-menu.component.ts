e3137f2f4c4d1a8484382f48da23b0d582262c06
Aditya26011 <adityapawar826@gmail.com>
2025-03-02 14:31:36 +0530

    ngFor Directive

--- src/app/header/mai-menu/mai-menu.component.ts
+++ src/app/header/mai-menu/mai-menu.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mai-menu',
   templateUrl: './mai-menu.component.html',
-  styleUrls: ['./mai-menu.component.css']
+  styleUrls: ['./mai-menu.component.css'],
 })
 export class MaiMenuComponent {
-
+  menuOptions: string[] = ['Home', 'Products', 'Contact', 'About'];
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,33 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  addToCart: number = 0;
-  product = {
-    name: 'iphone 13',
-    price: 999,
-    color: 'red',
-    discount: 8.5,
-    inStock: 5,
-    pImage: '/assets/Images/iphone.png',
-  };
-
-  onChange(event: any) {
-    // console.log(event.target.value);
-    // this.name = event.target.value;
-  }
-  getDiscountedPrice() {
-    return (
-      this.product.price - (this.product.price * this.product.discount) / 100
-    );
-  }
-  decrementCartValue() {
-    if (this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
-  incrementCartValue() {
-    if (this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
+  stringOfList: string[] = ['Mark', 'hanry', 'steve', 'clive'];
 }
