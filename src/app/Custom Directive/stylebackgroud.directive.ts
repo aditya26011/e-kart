ed1c7986272d2df66d06e0dcf7f8f1780155cd60
Aditya26011 <adityapawar826@gmail.com>
2025-04-08 22:25:02 +0530

    48- Renderer2 use instead of directly using nativeElement

--- src/app/Custom Directive/stylebackgroud.directive.ts
+++ src/app/Custom Directive/stylebackgroud.directive.ts
@@ -1,12 +1,17 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
 export class StyleBackground implements OnInit {
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.element.nativeElement.style.backgroundColor = '#36454f';
+    // this.element.nativeElement.style.backgroundColor = '#36454f';
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'background-color',
+      '#36454f'
+    );
   }
 }
