bc9ab0da66ae1a72e072a528e899f72070c52ab7
Aditya26011 <adityapawar826@gmail.com>
2025-04-12 12:00:37 +0530

    52- Property binding in directive

--- src/app/Custom Directive/stylebackgroud.directive.ts
+++ src/app/Custom Directive/stylebackgroud.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[setBackground]',
 })
 export class StyleBackground implements OnInit {
+  @Input() backColor: string = '#36454f';
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
@@ -11,7 +12,7 @@ export class StyleBackground implements OnInit {
     this.renderer.setStyle(
       this.element.nativeElement,
       'background-color',
-      '#36454f'
+      this.backColor
     );
   }
 }
